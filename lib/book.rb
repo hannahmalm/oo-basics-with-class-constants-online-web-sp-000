af36ab9b48b024e4b6b73e2a825d3c0758b60948
Hannah M <hannahemalm616@gmail.com>
2021-06-17 20:58:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,22 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
-  def initialize(title)
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre  
+  
+  GENRES = [] 
+  
+  def initialize (title)
     @title = title
-  end
-
-  def turn_page
+  end 
+  
+  def turn_page 
     puts "Flipping the page...wow, you read fast!"
-  end
+  end 
+  
+  #create the writer for genre and add the logic for the class constant
+    def genre=(genre)
+      @genre = genre
+      GENRES << genre  
+    end 
+  end 
 
-end
+  
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,29 @@
+require 'pry'
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :shoe
   attr_reader :brand
+  
+  BRANDS = [] 
+  
+  # def initialize(brand)
+  #   @brand = brand
+  # end 
+  
 
-  def initialize(brand)
-    @brand = brand
-  end
+  
+  def initialize (shoe)
+    @shoe = shoe
+  end 
+  
+  #create the writer for genre and add the logic for the class constant
+    def brand=(brand)
+      @brand = brand
+       BRANDS << brand.uniq  
+      
+    end 
+   
+  end 
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+  
 
-end
+  
